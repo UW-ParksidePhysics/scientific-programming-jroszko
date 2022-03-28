56d8bc98a9d9cf2fe5ae46868183d359691e387d
JRoszko <roszk009@rangers.uwp.edu>
2022-03-28 16:01:39 -0500

    Trajectory

--- python/Trajectory.py
+++ python/Trajectory.py
@@ -18,12 +18,14 @@ initial_velocity = initial_velocity * 3600 / 1000
 launch_angle = launch_angle * pi / 180
 
 
-def calculate_height(horizontal_displacement, angle, starting_velocity, starting_height):
+def calculate_height(horizontal_displacement, angle, starting_velocity, starting_height,
+                     gravitational_acceleration=9.80665):
     height = horizontal_displacement * tan(angle) \
-             - 1 / (2 * initial_velocity ** 2) * standard_gravity * horizontal_displacement ** 2 / ((cos(launch_angle)) ** 2) \
-             + initial_height
+             - 1 / (2 * starting_velocity ** 2) * gravitational_acceleration * horizontal_displacement ** 2 / ((cos(launch_angle)) ** 2) \
+             + starting_height
     return height
 
-
-projectile_height = calculate_height(horizontal_range, launch_angle, initial_velocity, initial_height)
-print(f'y     = {projectile_height:.1f} m')
+gravitational_accelerations = [3.7, 8.9, 9.8, 3.7, 23.1, 9.0, 8.7, 11.0]
+projectile_height = calculate_height(horizontal_range, launch_angle, initial_velocity, initial_height,
+                                     gravitational_acceleration=g)
+print(f'y(g={g:.1f} m/s^2)    = {projectile_height:.1f} m')

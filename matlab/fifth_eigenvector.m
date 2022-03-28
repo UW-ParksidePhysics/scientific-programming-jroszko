e69f9a660a784732fdfd1a9843824dc25b60595b
JRoszko <98778217+JRoszko@users.noreply.github.com>
2022-03-28 14:05:05 -0500

    Update fifth_eigenvector.m

--- matlab/fifth_eigenvector.m
+++ matlab/fifth_eigenvector.m
@@ -1,1 +1,14 @@
-% Test comment
+twos = 2*ones(5,1)
+diagonal_twos = diag(twos)
+negative_ones = -1*ones(4,1)
+upper_diag_ones = diag(negative_ones,1)
+lower_diag_ones = diag(negative_ones,-1)
+prefactor = 1/(2*(1/(5+1))^2)
+H = prefactor*(diagonal_twos+upper_diag_ones+lower_diag_ones)
+
+[eigen_vectors, eigen_values] = eig(H)
+xvalues = linspace(1/(5+1), 5/(5+1), 5);
+yvalues = -eigen_vectors(:,1)
+xvalues2 = linspace(0,1)
+yvalues2 = sqrt(2)*sin(pi*xvalues2)
+plot(xvalues,yvalues,xvalues2,yvalues2)

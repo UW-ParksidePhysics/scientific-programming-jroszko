173066f3d27f2256c10cc8853aea185e9dcacf4d
JRoszko <roszk009@rangers.uwp.edu>
2022-04-11 15:21:38 -0500

    tensile_stress_mini_project

--- matlab/tensile_stress_mini_project.m
+++ matlab/tensile_stress_mini_project.m
@@ -13,14 +13,14 @@
 %ground or hung in the air. Functions that need to be written have to relate to work (force times 
 %displacement) if an object is being pulled on the ground, or the force of gravity (mass times the 
 %gravity constant) to calculate the net force on the material that’s pulling an object. As a 
-%disclaimer, I will only be focusing on objects that are being pulled straight horizontally or hung 
-%straight vertically to simplify this project. I will be using vectors to store the values of work as 
+%disclaimer, this project will only be focusing on objects that are being pulled straight horizontally or hung 
+%straight vertically to simplify this project. This project will be using vectors to store the values of work as 
 %polar coordinates, as well as using arrays to store the values of displacement as an object is being 
-%pulled on the ground. As for data, I will need to read into physics textbooks I own and also use 
+%pulled on the ground. As for data, it will be necessary to read into physics textbooks and also use 
 %the internet as a free resource to look up how to calculate the cross-sectional area of ropes, 
 %chains, strings, etc., and to look up the equation of work involving friction. There is also a great 
 %article from Harvey Mudd College that explains the physics behind ropes, strings, and chains 
-%(https://www.math.hmc.edu/~dyong/papers/strings.pdf). I will visualize the results using charts 
+%(https://www.math.hmc.edu/~dyong/papers/strings.pdf). The results will be visualized by using charts 
 %of objects being pulled by chains/ropes and include arrows and other indicators to display the 
 %physics behind this problem.
 
@@ -28,9 +28,40 @@
 %	These are values particular to the simulation 
 %	that do not change later in the script
 
+% materials: cotton, hemp, etc.
+%youngs_moduli = [...(materials)];
+%yield_strength = [...(materials)];
+
+E_polyester = 2.9*10^9 % Young's modulus for Bulk Nylon
+yield_strength = 50*10^6
+maximum_strain = yield_strength/E_polyester
+deltaL = linspace(0, maximum_strain) % elastic strain at failure --> strength/Young's Modulus
+L = 10 % meters
+epsilon = deltaL ./ L
+E = E_polyester
+sigma = E .* epsilon
+plot(epsilon,sigma)
+hold on
+% E = young's modulus
+% sigma = stress
+% epsilon = strain
+E_nylon = 2.5*10^9 % Young's modulus for Bulk Nylon
+deltaL = (0:linspace(0, 25.2):25.2) % elastic strain at failure --> strength/Young's Modulus
+L = 10 % meters
+epsilon = deltaL ./ L
+E = E_nylon
+sigma = stress(epsilon, E)
+
+
+
+plot(epsilon,sigma)
+hold off
+
+
 % Computed parameters (from simulation parameters)
 %	These are values that do not change later in the script
 %	and are calculated from formulas using the simulation parameters
+
 
 % Function calls and simple calculations for:
 %	data read-in
@@ -41,6 +72,10 @@
 %	Each function contains help text: https://www.mathworks.com/help/matlab/matlab_prog/add-help-for-your-program.html
 
 %
-url = 'https://nssdc.gsfc.nasa.gov/planetary/factsheet/';
-data = webread(url);
-whos data
+%url = 'https://industrialrope.com/catalog-fiber-rope/cordage-comparative-weight-strength-and-working-load-chart/';
+%data = webread(url);
+%whos data
+
+function stresses = stress(strains, youngs_modulus)
+    stresses = youngs_modulus.*strains;
+end

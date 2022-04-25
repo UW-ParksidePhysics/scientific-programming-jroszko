c25d207150a872b146b80d4c760e2d1d32923f4b
William Parker <parkerw@uwp.edu>
2022-04-25 15:40:09 -0500

    tensile_stress_mini_project_python

--- python/tensile_stress_mini_project_python.py
+++ python/tensile_stress_mini_project_python.py
@@ -10,31 +10,38 @@ def strain(stresses, youngs_modulus):
     return stresses / youngs_modulus
 
 
+def read_webpage():
+    return webdata
+
+
+def calculate_stress_and_strain(youngs_moduli, yield_strengths):
+    stresses = []
+    strains = []
+    for index in range(len(youngs_moduli)):
+        yield_strength = yield_strengths[index]
+        E = youngs_moduli[index]
+        maximum_strain = strain(yield_strength, E)
+        deltaL = np.linspace(0, maximum_strain)
+        epsilon = deltaL / L
+        strains.append(epsilon)
+        sigma = E * epsilon
+        stresses.append(sigma)
+    return stresses, strains
+
+
+def plot_stress_strain(stresses, strains):
+    for epsilon, sigma in zip(strains, stresses):
+        plt.plot(epsilon, sigma)
+    plt.show()
+    return
+
+
 youngs_moduli = np.array([2.9 * 10 ** 9, 2.5 * 10 ** 9])
 yield_strengths = [50 * 10 ** 6, 63 * 10 ** 6]
 L = 10
-maximum_strain = 10
-deltaL = np.linspace(0, maximum_strain)
 
-for index in range(len(youngs_moduli)):
-    yield_strength = yield_strengths[index]
-    E = youngs_moduli[index]
-    maximum_strain = strain(yield_strength, E)
-E_polyester = 10
-maximum_strain = strain(yield_strength, E_polyester)
-deltaL = np.linspace(0, maximum_strain)
-L = 10
-epsilon = deltaL / L
-E = E_polyester
-sigma = E * epsilon
-plt.plot(epsilon, sigma)
 
-E_nylon = 2.5 * 10 ** 9
-deltaL = np.linspace(0, 25.2)
-L = 10
-epsilon = deltaL / L
-E = E_nylon
-sigma = stress(epsilon, E)
-plt.plot(epsilon, sigma)
-plt.show()
+
+
+
 

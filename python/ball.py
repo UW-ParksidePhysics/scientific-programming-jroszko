16e1076dbef01d552503b78d5c36300cd9402419
William Parker <parkerw@uwp.edu>
2022-04-25 16:19:11 -0500

    tensile_stress_mini_project_python

--- python/ball.py
+++ python/ball.py
@@ -1,3 +1,4 @@
 import vpython as vp
 
-vp.sphere()
+vp.sphere(pos=vp.vector(0, 0, 1), radius=0.5, color=vp.color.red)
+
---
+++ python/box.py
@@ -0,0 +1,2 @@
+from vpython import box, vector, color
+box(pos=vector(0, 0, 1), size=vector(0.3, 0.4, 0.5))

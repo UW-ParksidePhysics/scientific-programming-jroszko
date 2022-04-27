c5eb3d1c07c529eedd66670b5950d6b2e4c50f69
Jordan <roszk009@rangers.uwp.edu>
2022-04-26 22:42:59 -0500

    two_balls_cross

--- python/two_balls_cross.py
+++ python/two_balls_cross.py
@@ -1,8 +1,28 @@
 import vpython as vp
 
-initial_position = vp.vector(-1., 0., 0.)
-initial_velocity = vp.vector(1., 0., 0.)
-ball = vp.sphere(pos=initial_position, radius=0.1, color=vp.color.red, make_trail=True)
+initial_position = vp.vector(-3., 3., 3.)
+initial_velocity = vp.vector(1., -1., -1.)
+ball = vp.sphere(pos=initial_position,
+                 radius=0.5,
+                 color=vp.color.red,
+                 make_trail=True,
+                 trail_type="points",
+                 trail_radius=0.5,
+                 interval=15,
+                 retain=30)
+
+
+initial_position2 = vp.vector(7., 7., 7.)
+initial_velocity2 = vp.vector(-3., -3., -3.)
+ball2 = vp.sphere(pos=initial_position,
+                 radius=0.5,
+                 color=vp.color.blue,
+                 make_trail=True,
+                 trail_type="points",
+                 trail_radius=0.5,
+                 interval=15,
+                 retain=30)
+
 
 animation_time_step = 0.1  # seconds
 rate_of_animation = 1/animation_time_step
@@ -16,4 +36,10 @@ while time < stop_time:
     y = initial_position.y + initial_velocity.y * time
     z = initial_position.z + initial_velocity.z * time
     ball.pos = vp.vector(x, y, z)
+
+    vp.rate(rate_of_animation)
+    x2 = initial_position2.x + initial_velocity2.x * time
+    y2 = initial_position2.y + initial_velocity2.y * time
+    z2 = initial_position2.z + initial_velocity2.z * time
+    ball.pos = vp.vector(x2, y2, z2)
     time += time_step

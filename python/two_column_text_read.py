fb992a52dad9ad0d154442ee558ef933369e16d7
JRoszko <98778217+JRoszko@users.noreply.github.com>
2022-05-02 16:08:04 -0500

    Update two_column_text_read.py

--- python/two_column_text_read.py
+++ python/two_column_text_read.py
@@ -1,8 +1,14 @@
-def two_column_text_read(filename):
-  data = []
-  return data
+import numpy as np
+
+
+def two_column_text_read(file_name):
+    try:
+        data = np.loadtxt(file_name)
+        return data
+    except OSError:
+        print(f"{file_name} cannot be found")
 
 
 if __name__ == '__main__':
-  print('This is the test block')
-  
+    volume_energies = two_column_text_read("volume_energies.dat")
+    print(volume_energies)

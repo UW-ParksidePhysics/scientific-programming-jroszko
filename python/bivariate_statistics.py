84efca94c23a0f1a3238d660a9681c6227077a69
JRoszko <98778217+JRoszko@users.noreply.github.com>
2022-05-09 15:23:47 -0500

    Update bivariate_statistics.py

--- python/bivariate_statistics.py
+++ python/bivariate_statistics.py
@@ -1,8 +1,16 @@
+__author__ = "Pancho Rodriquez"
 import numpy as np
 from scipy import stats
 
 
 def bivariate_statistics(data):
+    """
+    Calculate statistical characteristics of a data set using SciPy's stats.describe function
+    :param data:ndarray, shape(2, M)
+    x-y data to be characterized. M is the number of data points.
+    :return: statistics: ndarray, shape (6,)
+    Mean of y, standard deviation of y, minimum x-value, maximum x-value, minimum y-value, maximum y-value
+    """
     if len(data) != 2 or len(data[0]) <= 1:
         raise IndexError("Inappropriate Dimensions")
     stat = stats.describe(data)
@@ -19,4 +27,3 @@ if __name__ == "__main__":
     y = [2, 4, 6]
     test_array = np.array([x, y])
     print(bivariate_statistics(test_array))
-

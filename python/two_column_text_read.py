700a2b9afe9b7ebb3b0528abcbd32dd647345e05
JRoszko <98778217+JRoszko@users.noreply.github.com>
2022-05-09 15:23:07 -0500

    Update two_column_text_read.py

--- python/two_column_text_read.py
+++ python/two_column_text_read.py
@@ -1,7 +1,15 @@
+__author__ = "Jack Binder"
 import numpy as np
 
 
 def two_column_text_read(file_name):
+    """
+    Read in two columns of data from a text file of arbitrary length
+    :param file_name:str
+    Name of file to be read in.
+    :return:data: ndarray, shape (2, M)
+    x-y data read in from file. M is the number of data points.
+    """
     try:
         data = np.loadtxt(file_name)
         return data

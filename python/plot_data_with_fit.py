4bd1a7bea7aef36208d265ec62aa98850c774727
JRoszko <98778217+JRoszko@users.noreply.github.com>
2022-05-08 20:49:25 -0500

    Update plot_data_with_fit.py

--- python/plot_data_with_fit.py
+++ python/plot_data_with_fit.py
@@ -1,4 +1,22 @@
-import numpy as py
+"""
+Takes in data from two_column_text_reaed and fit_curve_array from the eq.of state and plots the data.
+"""
+import matplotlib.pyplot as plt
 
-def plot_data_with_fit(file_name)
-    data = []
+
+def plot_data_with_fit(data, fit_curve, data_format="", fit_format=""):
+  
+  scatter_plot = plt.plot(data[0, :], data[1, :], data_format)
+  curve_plot = plt.plot(fit_curve[0, :], fit_curve[1, :], fit_format)
+  plt.title((r'Fm-3m Equation of State for Al in DFT GGA-PBE'), y=1.05)
+  plt.xlabel(r'$V$ $A^3/atom$')
+  plt.ylabel(r'$E$ $eV/atom$')
+
+  plt.xlim([90, 137])
+
+
+  plt.show()
+  
+  #plt.savefig("Initial_plot.png")
+
+  return scatter_plot, curve_plot

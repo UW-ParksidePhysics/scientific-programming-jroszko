0dc2f1e1ac6f222661e2b79b29e225a15e59ffc7
JRoszko <98778217+JRoszko@users.noreply.github.com>
2022-05-09 15:24:41 -0500

    Update quadratic_fit.py

--- python/quadratic_fit.py
+++ python/quadratic_fit.py
@@ -1,7 +1,16 @@
+__author__ = "Jack Binder"
 import numpy as np
 
 
-def bivariate_statistics(data):
+def quadratic_fit(data):
+    """
+    Fit a quadratic polynomial to a two row NumPy array of x-y data using NumPy's polyfit (Links to an external site.)
+    function
+    :param data: ndarray, shape (2, M)
+    x-y data to be fit. M is the number of data points.
+    :return:quadratic_coefficients: ndarray, shape (3,)
+    Quadratic polynomial coefficients, ordered quadratic term first, then linear term, and constant term last.
+    """
     xs = data[:, 0]
     ys = data[:, 1]
     quadratic_coefficients = np.polyfit(xs, ys, 2)
@@ -10,6 +19,5 @@ def bivariate_statistics(data):
 
 if __name__ == "__main__":
     test_data = np.array([[0, 0], [1, 1], [2, 4]])
-
-    coefficients = bivariate_statistics(test_data)
+    coefficients = quadratic_fit(test_data)
     print(coefficients)
